ee1e45584d463bcc3bc021b2fd661ba2505aa985
Guilherme Poglia <guilherme.poglia@edu.pucrs.br>
2024-08-31 04:39:38 -0300

    chore: Refactor backend directory structure

--- backend/infrastructure/database/connection.go
+++ backend/infrastructure/database/connection.go
@@ -1,4 +1,4 @@
-package initializerDB
+package database
 
 import (
 	"database/sql"
--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"hack/api"
+
+	"hack/infrastructure/database"
+	"os"
+)
+
+func main() {
+	db := database.StartDB()
+	defer db.Close()
+
+	myApp := api.NewApp(db)
+
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	api.RunServer(port, myApp)
+}
