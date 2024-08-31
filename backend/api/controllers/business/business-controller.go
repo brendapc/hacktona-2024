35f06de120158d11ab4b013bf5daf6e9a39bca00
Guilherme Poglia <guilherme.poglia@edu.pucrs.br>
2024-08-31 13:00:36 -0300

    feat: Add business controller and service for creating and retrieving businesses

---
+++ backend/api/controllers/business/business-controller.go
@@ -0,0 +1,49 @@
+package businessController
+
+import (
+	"hack/core/models"
+	service "hack/core/services/business"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BusinessController struct {
+	businessService service.BusinessService
+}
+
+func (c *BusinessController) CreateBusiness(ctx *gin.Context) {
+	var business models.Business
+
+	if err := ctx.BindJSON(&business); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.businessService.CreateBusiness(ctx, &business); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, business)
+}
+
+func (c *BusinessController) GetBusinesses(ctx *gin.Context) {
+	businesses, err := c.businessService.GetBusinesses(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, businesses)
+}
+
+func Handler(router *gin.RouterGroup, businessService service.BusinessService) *BusinessController {
+	controller := &BusinessController{
+		businessService: businessService,
+	}
+
+	router.POST("/business", controller.CreateBusiness)
+
+	return controller
+}
---
+++ backend/core/services/business/business-service.go
@@ -0,0 +1,45 @@
+package business
+
+import (
+	"context"
+	"database/sql"
+	"hack/core/models"
+
+	"log"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+type BusinessService struct {
+	db *sql.DB
+}
+
+type BusinessServiceInterface interface {
+	CreateBusiness(ctx context.Context, business *models.Business) error
+}
+
+func NewService(db *sql.DB) BusinessService {
+	return BusinessService{
+		db: db,
+	}
+}
+
+func (s *BusinessService) CreateBusiness(ctx context.Context, business *models.Business) error {
+
+	err := business.Insert(ctx, s.db, boil.Infer())
+	if err != nil {
+		log.Printf("Error inserting business into database: %v", err)
+		return err
+	}
+	return nil
+
+}
+
+func (s *BusinessService) GetBusinesses(ctx context.Context) ([]*models.Business, error) {
+	businesses, err := models.Businesses().All(ctx, s.db)
+	if err != nil {
+		log.Printf("Error fetching businesses from database: %v", err)
+		return nil, err
+	}
+	return businesses, nil
+}
