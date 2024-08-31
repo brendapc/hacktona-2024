5a1f8585ceda0e8aad38ca01dbc222bb40530102
Leonardo Bertoletti <Leonardo.Bertoletti@edu.pucrs.br>
2024-08-31 07:18:05 -0300

    usestate da tela te registro de empresa

--- frontend/src/pages/CompanyRegisterScreen.jsx
+++ frontend/src/pages/CompanyRegisterScreen.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import ButtonComponent from "../components/ButtonComponent";
 import InputApp from "../components/input/input";
 
 export default function CompanyRegister() {
+    const [compName,SetCompName] = useState('')
+    const [cnpj,SetCnpj] = useState('')
+    const [address,SetAddress] = useState('')
+    const [phone,SetPhone] = useState('')
+    const [email,SetEmail] = useState('')
+    const [password,SetPassword] = useState('')
     return (
         <div className="flex items-center justify-center min-h-screen bg-custom-gradient">
             <div className="w-5/6 flex-row p-16 space-y-14 bg-[#F5F9E9] rounded-2xl shadow-lg">
@@ -11,15 +18,15 @@ export default function CompanyRegister() {
                     <div className="w-1/2 px-6">
                         <div className="w-full mb-3 space-y-1">
                             <label>Nome da empresa:</label>
-                            <InputApp placeHolder="" />
+                            <InputApp onChangeText={SetCompName}/>
                         </div>
                         <div className="w-full mb-3 space-y-1">
                             <label>CNPJ:</label>
-                            <InputApp placeHolder="" />
+                            <InputApp onChangeText={SetCnpj}/>
                         </div>
                         <div className="w-full mb-3 space-y-1">
                             <label>Endereço:</label>
-                            <InputApp placeHolder="" />
+                            <InputApp onChangeText={SetAddress}/>
                         </div>
                     </div>
 
@@ -27,20 +34,20 @@ export default function CompanyRegister() {
                     <div className="w-1/2 px-6">
                         <div className="w-full mb-3 space-y-1">
                             <label>Telefone:</label>
-                            <InputApp placeHolder="" />
+                            <InputApp onChangeText={SetPhone}/>
                         </div>
                         <div className="w-full mb-3 space-y-1">
                             <label>Email:</label>
-                            <InputApp placeHolder="" />
+                            <InputApp onChangeText={SetEmail}/>
                         </div>
                         <div className="w-full mb-3 space-y-1">
                             <label>Senha:</label>
-                            <InputApp placeHolder="" />
+                            <InputApp onChangeText={SetPassword}/>
                         </div>
                     </div>
                 </div>
                 <div className="flex justify-center">
-                    <ButtonComponent label={"Cadastrar"} />
+                    <ButtonComponent label={"Cadastrar"} onClick={()=>{}} />
                 </div>
             </div>
         </div>
