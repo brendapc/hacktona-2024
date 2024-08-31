d50c96749fb432d9e18dde9bb02df723a2894f30
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-08-31 07:57:56 -0300

    wip: perfil de abrigo

---
+++ frontend/src/components/ShelterHeader.jsx
@@ -0,0 +1,13 @@
+export const ShelterHeader = () => {
+    return (
+        <header className=" bg-white/70">
+            <nav>
+                <ul className="flex p-2 space-x-10" > 
+                    <li className="bg-[#c9c9c9c9] rounded-xl py-1 px-4" >Minha conta</li>
+                    <li className="bg-[#c9c9c9c9] rounded-xl py-1 px-4">Cadastrar abrigados</li>
+                    <li className="bg-[#c9c9c9c9] rounded-xl py-1 px-4">Banco de voluntários</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ frontend/src/pages/ShelterProfile/ShelterProfile.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { ShelterHeader } from "../../components/ShelterHeader"
+
+export const ShelterProfile = () => {
+    return (
+        <div className="bg-custom-gradient w-screen h-screen overflow-scroll">
+        <ShelterHeader/>
+        </div>
+    )
+}
