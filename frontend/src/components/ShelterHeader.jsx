a8930670e664504d2d2afbb32732f060936e39e5
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-08-31 08:10:10 -0300

    feat: header de perfil de abrigo

--- frontend/src/components/ShelterHeader.jsx
+++ frontend/src/components/ShelterHeader.jsx
@@ -1,11 +1,17 @@
 export const ShelterHeader = () => {
     return (
-        <header className=" bg-white/70">
+        <header className="bg-white/70">
             <nav>
-                <ul className="flex p-2 space-x-10" > 
-                    <li className="bg-[#c9c9c9c9] rounded-xl py-1 px-4" >Minha conta</li>
-                    <li className="bg-[#c9c9c9c9] rounded-xl py-1 px-4">Cadastrar abrigados</li>
-                    <li className="bg-[#c9c9c9c9] rounded-xl py-1 px-4">Banco de voluntários</li>
+                <ul className="flex p-2 space-x-10">
+                    <li className="p-2">
+                        <a className="bg-[#c9c9c9] rounded-xl py-1 px-6 hover:bg-[#b0b0b0] focus:bg-[#a0a0a0] font-bold" href="/minha-conta">Minha conta</a>
+                    </li>
+                    <li className="p-2">
+                        <a className="bg-[#c9c9c9] rounded-xl py-1 px-4 hover:bg-[#b0b0b0]  focus:bg-[#a0a0a0]  font-bold" href="/cadastrar-abrigados">Cadastrar abrigados</a>
+                    </li>
+                    <li className="p-2">
+                        <a className="bg-[#c9c9c9] rounded-xl py-1 px-4 hover:bg-[#b0b0b0] focus:bg-[#a0a0a0 font-bold" href="/banco-de-voluntarios">Banco de voluntários</a>
+                    </li>
                 </ul>
             </nav>
         </header>
