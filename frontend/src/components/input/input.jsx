2e8b01d20d106b3a2522094fc10e9e22e87708d9
Mauricio Krziminski <mauricio.krziminski@edu.pucrs.br>
2024-08-31 04:51:41 -0300

    chore: Update input component styling and add placeholder color

--- frontend/src/components/input/input.jsx
+++ frontend/src/components/input/input.jsx
@@ -1,7 +1,8 @@
 export const InputApp = ({placeHolder,onChangeText}) => {
     return (
       
-    <input placeholder={placeHolder} onChange={(e)=>onChangeText(e.target.value)} className="w-5/6 drop-shadow-lg rounded-md p-2 mx-2"/>
+    <input placeholder={placeHolder} onChange={(e)=>onChangeText(e.target.value)} className="w-full drop-shadow-lg rounded-md h-10 px-4 py-2 placeholder-gray-400 border-none shadow-md focus:outline-none focus:ring-2 focus:ring-[#74D480]"/>
       
     )
   }
+  export default InputApp;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,30 +1,18 @@
+import InputApp from "../components/input/input";
+
 export const LoginPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-custom-gradient">
       <div className="w-full max-w-md p-8 space-y-6 bg-[#F5F9E9] rounded-2xl shadow-lg">
         <h2 className="text-2xl font-bold text-center text-black">Login</h2>
         
-        <form className="space-y-4">
+        <form className="space-y-6">
           <div>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              required
-              className="w-full px-4 py-2 mt-1 text-gray-700 placeholder-gray-400 bg-white border-none rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-[#74D480]"
-              placeholder="email@email.com"
-            />
+            <InputApp placeHolder="Digite seu e-mail" />
           </div>
 
           <div>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              required
-              className="w-full px-4 py-2 mt-1 text-gray-700 placeholder-gray-400 bg-white border-none rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-[#74D480]"
-              placeholder="senha"
-            />
+            <InputApp placeHolder="Digite sua senha" />
           </div>
 
           <div>
