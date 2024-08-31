0a65b1cc230eb67626bbd4c1b00c1d103b12abb2
Lucas Vieira <lpvieira03@gmail.com>
2024-08-31 07:00:49 -0300

    ajustando tam cardButton

--- frontend/src/components/CardButtonComponent.jsx
+++ frontend/src/components/CardButtonComponent.jsx
@@ -2,7 +2,7 @@ export const CardButtonComponent = ({ onClick, label, title, image }) => {
     return (
         <button
             onClick={onClick}
-            className="w-64 h-80 rounded-lg bg-[#F5FCE8] overflow-hidden shadow-lg border text-center flex flex-col items-center justify-between p-2"
+            className="w-72 h-96 rounded-lg bg-[#F5FCE8] overflow-hidden shadow-lg border text-center flex flex-col items-center justify-between p-2"
         >
             <div className="text-lg font-semibold">{title}</div>
             <div className="flex-grow flex items-center justify-center">
