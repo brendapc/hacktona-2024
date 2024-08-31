23439d30c65f704fec9ce9fcfaeb326606c61cfa
Mauricio Krziminski <mauricio.krziminski@edu.pucrs.br>
2024-08-31 05:25:12 -0300

    refactor: Update LoginPage layout and styling

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,15 +4,15 @@ import ButtonComponent from "../components/ButtonComponent";
 export const LoginPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-custom-gradient">
-      <div className="w-full max-w-md p-8 space-y-6 bg-[#F5F9E9] rounded-2xl shadow-lg">
-        <h2 className="text-2xl font-bold text-center text-black">Login</h2>
+      <div className="w-full max-w-lg p-16 space-y-14 bg-[#F5F9E9] rounded-2xl shadow-lg">
+        <h2 className="text-3xl font-bold text-center text-black">Login</h2>
         
-        <form className="space-y-6">
-          <div>
+        <form className="flex flex-col items-center w-full">
+          <div className="w-full mb-6 mt-4">
             <InputApp placeHolder="Digite seu e-mail" />
           </div>
 
-          <div>
+          <div className="w-full mb-32">
             <InputApp placeHolder="Digite sua senha" />
           </div>
 
@@ -21,7 +21,7 @@ export const LoginPage = () => {
           </div>
         </form>
 
-        <div className="text-center">
+        <div className="flex flex-col items-center text-center">
           <p className="text-sm mb-2 text-gray-600">
             Não possui conta?
           </p>
