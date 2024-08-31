1d8cc6a65026e6bf694715765f6bb60e1336f7ae
Lucas Vieira <lpvieira03@gmail.com>
2024-08-31 11:41:20 -0300

    aviso de campo preenchido  incorretamente no login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,20 +3,41 @@ import ButtonComponent from "../components/ButtonComponent";
 import { useState } from "react";
 
 export const LoginPage = () => {
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!email) newErrors.email = "Email é obrigatório.";
+    else if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = "Email inválido.";
+    if (!password) newErrors.password = "Senha é obrigatória.";
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    if (validateForm()) {
+      console.log("Logado Com Sucesso!");
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-custom-gradient">
       <div className="w-full max-w-lg p-16 space-y-14 bg-[#F5F9E9] rounded-2xl shadow-lg">
         <h2 className="text-3xl font-bold text-center text-black">Login</h2>
         
-        <form className="flex flex-col w-full">
+        <form className="flex flex-col w-full" onSubmit={handleSubmit}>
           <div className="w-full mb-6 mt-4">
-            <InputApp placeHolder="Email" onChangeText={setEmail}  />
+            <InputApp placeHolder="Email" onChangeText={setEmail} />
+            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
           </div>
 
           <div className="w-full mb-32">
             <InputApp placeHolder="Senha" type="password" onChangeText={setPassword} />
+            {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
           </div>
 
           <div className="flex justify-center">
