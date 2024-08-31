d4bf87d54e1143cb2dfdbde2e07392731e097364
jpsalles21 <joaopedrosalles.dev@gmail.com>
2024-08-31 08:50:08 -0300

    feat: pagina para adicionar e remover abrigados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import { LoginPage } from './pages/LoginPage';
 import Abrigos from './pages/Shelter';
 import { ShelterProfile } from './pages/ShelterProfile/ShelterProfile';
+import { ShelteredForm } from './pages/ShelterProfile/ShelteredForm';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/abrigos" element={<Abrigos />} />
           <Route path="/perfil-abrigo" element={<ShelterProfile />} />
+          <Route path="/cadastro-abrigados" element={<ShelteredForm />} />
         </Routes>
       </div>
     </Router>
